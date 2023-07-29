e67f3c4ef94789aafc84e8cc02c09aac85cd38ee
PabaleloLucia <48206169+PabaleloLucia@users.noreply.github.com>
2023-07-29 17:17:35 +0200

    added the app.css file

--- src/Weather.js
+++ src/Weather.js
@@ -2,113 +2,111 @@ import React from "react";
 import "./Weather.css";
 
 export default function Weather() {
+  let weatherData = {
+    city: "Gaborone",
+    temperature: 19,
+    date: "Tuesday",
+    time: "11:00",
+    description: "Windy",
+    imgUrl: "https://ssl.gstatic.com/onebox/weather/64/windy.png",
+    humidity: 80,
+    wind: 10,
+  };
+
   return (
-    <div className="Weather">
-      <div className="card">
-        <div className="container">
-          <div className="wrapper">
-            <div className="the-app">
-              <form
-                className="row row-cols-lg-auto g-3 align-items-center mb-4"
-                id="city-form"
-              >
-                <div className="col-6">
-                  <input
-                    type="search"
-                    autocomplete="off"
-                    autofocus="on"
-                    className="form-control"
-                    id="cityWeatherForecast"
-                    placeholder="Enter city name"
-                  />
-                </div>
-                <div className="col-3">
-                  <input
-                    type="submit"
-                    value="Search"
-                    className="btn btn-outline-secondary w-100"
-                  />
-                </div>
-                <div className="col-3">
-                  <input
-                    type="submit"
-                    value="Current"
-                    id="current-location"
-                    className="btn btn-outline-secondary w-100"
-                  />
-                </div>
-              </form>
-
-              <div className="weather">
-                <h1 id="city">Gaborone</h1>
-                <ul>
-                  <li>
-                    Today: <span id="date"> 13:00</span>
-                  </li>
-                  <li id="description"> windy</li>
-                </ul>
-              </div>
-              <div className="row">
-                <div className="col-6">
-                  <div className="clearfix temp">
-                    <i class="fa-solid fa-wind"></i>
-                    <div className="float-left">
-                      <strong id="temperature">12</strong>
-                      <span className="weather-units">
-                        <a
-                          type="button"
-                          className="btn btn-link focus"
-                          id="key2"
-                        >
-                          ℃
-                        </a>{" "}
-                        |
-                        <a type="button" className="btn btn-link " id="key">
-                          ℉
-                        </a>
-                      </span>
-                    </div>
-                  </div>
-                </div>
-                <div className="col-6">
-                  <ul>
-                    <li>
-                      Humidity: <span id="humidity">3</span>%
-                    </li>
-                    <li>
-                      Wind: <span id="wind">21</span>km/h
-                    </li>
-                  </ul>
-                </div>
-              </div>
-              <div className="app-forecast" id="weather-forecast"></div>
+    <div className="container">
+      <div className="weather">
+        <form>
+          <div className="row">
+            <div className="col-6">
+              <input
+                type="search"
+                placeholder="Enter a city name..."
+                autocomplete="on"
+                className="form-control shadow-sm"
+              />
             </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search"
+                className="form-control btn btn-light shadow-sm"
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Current"
+                id="current-location"
+                className="form-control btn btn-light shadow-sm"
+              />
+            </div>
+          </div>
+        </form>
 
+        <h1>{weatherData.city}</h1>
+        <ul>
+          <li>
+            Last updated: {weatherData.date}{" "}
+            <span id="date-time">{weatherData.time}</span>
+          </li>
+          <li>{weatherData.description}</li>
+        </ul>
+        <div className="row">
+          <div className="col-4">
+            <div className="clearfix">
+              <img
+                src={weatherData.imgUrl}
+                alt={weatherData.description}
+                className="float-left"
+              />
+            </div>
+          </div>
+          <div className="col-4">
+            <div className="float-left">
+              <strong>{weatherData.temperature}</strong>
+              <span className="units">
+                <a href="/" className="active">
+                  °C
+                </a>{" "}
+                | <a href="/">°F</a>
+              </span>
+            </div>
+          </div>
+
+          <div className="col-4">
+            <ul>
+              <li> Humidity: {weatherData.humidity}%</li>
+              <li> Wind: {weatherData.wind} km/h</li>
+            </ul>
+          </div>
+          <hr />
+          <p class="githubLink">
             <small>
               Open Source Code on{" "}
               <a
+                class="gitHub"
                 href="https://github.com/PabaleloLucia/My-Project.git"
-                rel="noreferrer"
-                title="Project on GitHub"
                 target="_blank"
-                className="btn btn-link "
+                rel="noreferrer"
               >
-                GitHub
+                Github
               </a>{" "}
-              by{" "}
+              by
               <a
+                class="email"
                 href="mailto:pabalelotsebempe@gmail.com"
                 target="_blank"
-                className="btn btn-link "
-                title="email me"
                 rel="noreferrer"
               >
+                {" "}
                 Pabalelo Tsebempe
               </a>
+              <span id="theme_toggler"> Change theme </span>
             </small>
-          </div>
+          </p>
         </div>
-      </div>
+      </div>{" "}
     </div>
   );
 }
